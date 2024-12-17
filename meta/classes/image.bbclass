mattih11/isar

commit 8c107103ac3d9f41795aba69eda36c3e0dfc5c27

Fix rootfs source list handling in image.bbclass
Ensure that the APT sources list is only moved if both the sources-list
file and the sources.list.d directory exist. This prevents errors when
APT is not installed, as the sources.list.d directory will be missing.

Signed-off-by: Vidyasagar G C <vidyasagar.gc@siemens.com>

meta/classes/image.bbclass
@@ -419,7 +419,7 @@ do_rootfs_finalize() {
                 -maxdepth 1 -name 'qemu-*-static' -type f -delete
         fi
 
-        if [ -e "${ROOTFSDIR}/etc/apt/sources-list" ]; then
+        if [ -e "${ROOTFSDIR}/etc/apt/sources-list" ] && [ -e "${ROOTFSDIR}/etc/apt/sources-list.d" ]; then
             mv "${ROOTFSDIR}/etc/apt/sources-list" \
                 "${ROOTFSDIR}/etc/apt/sources.list.d/bootstrap.list"
         fi

commit 8e2832284b149268c06ec934265db8489ec87d16

meta/classes/image: move sources-list as bootstrap.list in rootfs_finalize
Presently, sources-list is not moved as bootstrap.list, even though the source-list file
and sources.list.d directory present, this regression introduced in 8c10710

Make sure to use -d if the directory exists and use the correct name of the directory (sources.list.d).

Signed-off-by: srinuvasan <srinuvasan.a@siemens.com>

meta/classes/image.bbclass
@@ -416,7 +416,7 @@ do_rootfs_finalize() {
                 -maxdepth 1 -name 'qemu-*-static' -type f -delete
         fi
 
-        if [ -e "${ROOTFSDIR}/etc/apt/sources-list" ] && [ -e "${ROOTFSDIR}/etc/apt/sources-list.d" ]; then
+        if [ -e "${ROOTFSDIR}/etc/apt/sources-list" ] && [ -d "${ROOTFSDIR}/etc/apt/sources.list.d" ]; then
             mv "${ROOTFSDIR}/etc/apt/sources-list" \
                 "${ROOTFSDIR}/etc/apt/sources.list.d/bootstrap.list"
         fi